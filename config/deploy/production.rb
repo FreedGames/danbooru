d27fdd5dfe5606c4f9cb68ec6188ddfb5071cfbe
FreedGames <contact.freedgames@gmail.com>
2017-02-13 21:34:23 +0100

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :user, "albert"
+set :user, "freed"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "freed", :roles => %w(web app db), :primary => true, :user => "freed"
+server "freed", :roles => %w(web app), :user => "freed"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
